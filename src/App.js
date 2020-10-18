26f4a1961c47abe9e42fb5d5bf4d6b4977c04ade
simanicmarko <marezver023@gmail.com>
2020-10-18 22:11:25 +0200

    added redux to project and completed user reducer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import {connect} from 'react-redux'
+import {setCurrentUser} from './redux/user/user.actions'
 
 
 const HatsPage = () =>
@@ -17,36 +19,28 @@ const HatsPage = () =>
 )
 
 class App extends React.Component{
-    constructor(){
-      super();
-
-      this.state = {
-          currentUser : null
-      }
-    }  
     
     unsubscribeFromAuth = null 
 
     componentDidMount() {
+
+      const {setCurrentUser} = this.props
+
       this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
         if (userAuth) {
           const userRef = await createUserProfileDocument(userAuth);
   
           userRef.onSnapshot(snapShot => {
-            this.setState({
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            });
-              //currentUser
-            //console.log(this.state);
+            setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
+            })
           });
         }
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth)
       });
     }
-
+ 
     componentWillUnmount(){
       this.unsubscribeFromAuth()
     }
@@ -54,7 +48,7 @@ class App extends React.Component{
     render(){
         return (
           <div>
-            <Header  currentUser = {this.state.currentUser}/>
+            <Header/>
             <Switch> 
                 <Route exact path='/' component={HomePage}></Route>
                 <Route exact path='/hats' component={HatsPage}></Route>
@@ -67,4 +61,8 @@ class App extends React.Component{
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser : user => dispatch(setCurrentUser(user ))
+})
+
+export default connect(null,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
+import store from './redux/store';
+import {Provider} from 'react-redux'
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
